46621065d8698f0f06b45b10f7b314eec14b6676
Jack <jack@raksmart.com>
2025-08-11 11:47:15 +0800

    update routes
    Signed-off-by: Jack <jack@raksmart.com>

--- pkg/api/resources/adapters/ipaddress.go
+++ pkg/api/resources/adapters/ipaddress.go
@@ -216,9 +216,8 @@ func (a *AddressAdapter) getAddressResponse(ctx context.Context, address *model.
 			floatingIp, err = a.floatingIpService.GetFloatingIpByAddress(ctx, address.Address)
 			if err != nil {
 				logger.Errorf("Failed to get floating IP for interface %d, err: %v", iface.ID, err)
-				return
 			}
-			if floatingIp.Instance != nil {
+			if err == nil && floatingIp.Instance != nil {
 				owner = orgAdmin.GetOrgName(ctx, floatingIp.Instance.Owner)
 				addressResp.TargetInterface.FromInstance = &InstanceInfo{
 					ResourceReference: &ResourceReference{
